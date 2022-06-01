4a811761bacd1e577b9c8cd1331983151f94349e
GalyaIvanova <32123443+GalyaIvanova@users.noreply.github.com>
2022-06-01 16:16:28 +0300

    Update MN.java

--- src/com/company/MN.java
+++ src/com/company/MN.java
@@ -3,32 +3,75 @@ package com.company;
 import java.util.LinkedList;
 
 public class MN {
-
+private static int StepCounter=0;
     public static void main(String args[])
     {
+        int m=5;
+        int n=5;
 
-     int arr[][]=new int[5][5];
-     int x=2;
-     boolean found=false;
-        for(int i=0;i<5;i++) {
+        int x=8;
+        
+        int arr[][]=new int[m][n];
+        
+        
+//        инициализация и принтване на масива
+//        int num=1;
+//        for (int i=0;i<m;i++){
+//            for (int j=0;j<n;j++){
+//                arr[i][j]=num;
+//                num++;
+//            }
+//        }
+//
+//        for (int i=0;i<m;i++){
+//            for (int j=0;j<n;j++){
+//                System.out.print( arr[i][j]+" ");
+//
+//            }
+//            System.out.println('\n');
+//        }
 
-            for (int j = i; j < 5; j++) {
+
+     
+        boolean found=false;
+
+        for(int i=0;i<(m-1);i++) {
+            if(arr[0][0]>x || arr[m-1][n-1]<x)break;
+
+            for (int j = i; j < (n-1); j++) {
+                StepCounter++;
+
                 if(arr[i][j]==x){
-                    System.out.println(i+" "+j);
                     found=true;
                     break;
                 }
-                if(arr[i+1][j+1]<=x){
-                    i++;
-                    continue;
-                }
 
-                if(arr[i][j+1]<=x){
+                if(arr[i][j+1]<=x) {
+                    
+                    if(i!=(m-1)){
+                        if(arr[i+1][j+1]<=x){
+                            if(arr[i+1][j+1]==x){
+                                found=true;
+                                break;
+                            }
+                            break;
+                        }
+                    }
+                    if (arr[i][j + 1] == x) {
+                        found = true;
+                        break;
+                    }
+                        if((j+1)==(n-1)){
+                        j=-1;
+                        i++;
+                    }
                     continue;
                 }
             }
-           if(found)break;
+            if(found)break;
         }
+
+        System.out.println("Steps needed: "+StepCounter +" "+found);
 
     }
 }
